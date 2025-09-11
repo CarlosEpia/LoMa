bb9daa8000ac52ad4e50548561ccf3f449df4567
CarlosEpia <caepiar@unal.edu.co>
2025-09-11 11:18:52 +0200

    use black

--- loma/appl.py
+++ loma/appl.py
@@ -13,53 +13,82 @@ from demands.import_household_demand import distribute_household_demand
 from network.import_network_from_shape_files import create_pypsa_network
 from demands.import_EV_demand import import_EV_loads
 from demands.import_hp_demand import add_heat_loads_to_network
-from network.flexibilities_14a_heat_pump import insert_heat_pump_flexibilities_14a
-from constraints.constraints import  load_reduction_constraint_14a
-from generators.generators import insert_pv_rooftop_and_battery
+from network.flexibilities_14a_heat_pump import (
+    insert_heat_pump_flexibilities_14a,
+)
+from constraints.constraints import load_reduction_constraint_14a
+from pv_rooftop_and_home_battery.pv_rooftop_and_home_battery import (
+    insert_pv_rooftop_and_battery,
+)
 
 args = {
-        "path_to_shapefiles_grid": 'data/Input_files/Filtered_data_Kronenburg_V3',  # define path of shapefiles for grid infrastructure (related to execution folder)
-        "path_to_shapefile_MV_grid": 'data/Input_files/MV_grid_district/husum_district.shp',  #define path of shapefiles for boundaries of husum_district 
-        "nuts3_focus_region": "Nordfriesland, Schleswig-Holstein, Germany",  
-        "path_to_household_data": "data/Input_files/quantity_household_fixed.csv",
-        "path_to_heat_pump_data": "data/Input_files/heat_pumps.csv",
-        'batteries_path': 'data/data_bundle/generators_and_batteries/batt_SH.geojson',
-        'pv_rooftop_path': 'data/data_bundle/generators_and_batteries/rooftop_SH.geojson',
-        "Kabeltypen": {
-            "NAYY 4x240": {"U": 400, "I_max": 364, "R": 0.125, "L": 0.254},     #values based on FaberKabel Starkstromkabel NAYY-J/-O nach VDE 0276-603 (same as dingO-grid-values)
-            "NAYY 4x150": {"U": 400, "I_max": 275, "R": 0.206, "L": 0.256},     # U[v], I[A], R[Ohm/km], L[mH/km]
-            "NAYY 4x95": {"U": 400, "I_max": 215, "R": 0.206, "L": 0.261},
-            "NAYY 4x35": {"U": 400, "I_max": 123, "R": 0.868, "L": 0.271}
-                    }
-        }
+    "path_to_shapefiles_grid": "data/Input_files/Filtered_data_Kronenburg_V3",  # define path of shapefiles for grid infrastructure (related to execution folder)
+    "path_to_shapefile_MV_grid": "data/Input_files/MV_grid_district/husum_district.shp",  # define path of shapefiles for boundaries of husum_district
+    "nuts3_focus_region": "Nordfriesland, Schleswig-Holstein, Germany",
+    "path_to_household_data": "data/Input_files/quantity_household_fixed.csv",
+    "path_to_heat_pump_data": "data/Input_files/heat_pumps.csv",
+    "batteries_path": "data/data_bundle/generators_and_batteries/batt_SH.geojson",
+    "pv_rooftop_path": "data/data_bundle/generators_and_batteries/rooftop_SH.geojson",
+    "Kabeltypen": {
+        "NAYY 4x240": {
+            "U": 400,
+            "I_max": 364,
+            "R": 0.125,
+            "L": 0.254,
+        },  # values based on FaberKabel Starkstromkabel NAYY-J/-O nach VDE 0276-603 (same as dingO-grid-values)
+        "NAYY 4x150": {
+            "U": 400,
+            "I_max": 275,
+            "R": 0.206,
+            "L": 0.256,
+        },  # U[v], I[A], R[Ohm/km], L[mH/km]
+        "NAYY 4x95": {"U": 400, "I_max": 215, "R": 0.206, "L": 0.261},
+        "NAYY 4x35": {"U": 400, "I_max": 123, "R": 0.868, "L": 0.271},
+    },
+}
 
-#create pypsa network with grid topology shapefilees
-n = create_pypsa_network(args['path_to_shapefiles_grid'], args['path_to_household_data'], args['path_to_heat_pump_data'], args['Kabeltypen'])
+# create pypsa network with grid topology shapefilees
+n = create_pypsa_network(
+    args["path_to_shapefiles_grid"],
+    args["path_to_household_data"],
+    args["path_to_heat_pump_data"],
+    args["Kabeltypen"],
+)
 
 #insert solar_rooftop and home_batteries
-n = insert_pv_rooftop_and_battery(n, args['path_to_shapefile_MV_grid'], args['pv_rooftop_path'], args['batteries_path'],)
+n = insert_pv_rooftop_and_battery(
+    n,
+    args["path_to_shapefile_MV_grid"],
+    args["pv_rooftop_path"],
+    args["batteries_path"],
+)
 
-#insert cts demand
-n = inser_cts_demand_per_building(n, args['path_to_shapefile_MV_grid'])
+# insert cts demand
+n = inser_cts_demand_per_building(n, args["path_to_shapefile_MV_grid"])
 
-#insert industrial demands
-n = insert_ind_demand_per_building(n,  args['path_to_shapefile_MV_grid'], args['nuts3_focus_region'])
+# insert industrial demands
+n = insert_ind_demand_per_building(
+    n, args["path_to_shapefile_MV_grid"], args["nuts3_focus_region"]
+)
 
-#household-type distribution on 100x100m             ###ToDo: combine create_household_dist and distribute_household_demand
-household_dist_df = create_household_dist(args['path_to_shapefile_MV_grid'])
+# household-type distribution on 100x100m             ###ToDo: combine create_household_dist and distribute_household_demand
+household_dist_df = create_household_dist(args["path_to_shapefile_MV_grid"])
 
-#allocate profiles to buses
+# allocate profiles to buses
 n = distribute_household_demand(n, household_dist_df)
 
-#insert_heat_loas_for_heat_pump_location
+# insert_heat_loas_for_heat_pump_location
 n = add_heat_loads_to_network(n)
 
-#insert EV_loads
-n = import_EV_loads(n, args['path_to_shapefiles_grid'])
+# insert EV_loads
+n = import_EV_loads(n, args["path_to_shapefiles_grid"])
 
-#insert heat pump flexibilities
+# insert heat pump flexibilities
 n = insert_heat_pump_flexibilities_14a(n)
 
-#Optimize
-n.optimize(snapshots=n.snapshots[:24], solver_name='glpk', extra_functionality=load_reduction_constraint_14a)
-
+# Optimize
+n.optimize(
+    snapshots=n.snapshots[:24],
+    solver_name="glpk",
+    extra_functionality=load_reduction_constraint_14a,
+)
