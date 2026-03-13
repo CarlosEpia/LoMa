9f8da52fcd346b21dbb517762b030e7a1ab7d0de
Lennart Zimmermann <lennartzimmermann@outlook.de>
2026-03-13 08:36:58 +0100

    Implement scenario creation for pv and home-batteries

--- loma/pv_rooftop_and_home_battery/pv_rooftop_and_home_battery.py
+++ loma/pv_rooftop_and_home_battery/pv_rooftop_and_home_battery.py
@@ -5,19 +5,19 @@ import logging
 
 
 def insert_pv_rooftop_and_battery(
-    network, shape_path, pv_rooftop_path, pv_feedin_path, batteries_path
+    network, shape_path, pv_rooftop_path, pv_feedin_path, batteries_path, scenario
 ):
     shape = gpd.read_file(shape_path).to_crs(32632)
     buses = network.buses.copy()
     buses = buses[buses["comp_type"] == "house_connection"]
     buses = gpd.GeoDataFrame(buses, geometry="geom", crs=32632)
-    network = insert_pv_rooftop(network, shape, buses, pv_rooftop_path, pv_feedin_path)
-    network = insert_home_battery(network, shape, buses, batteries_path)
+    network = insert_pv_rooftop(network, shape, buses, pv_rooftop_path, pv_feedin_path, scenario)
+    network = insert_home_battery(network, shape, buses, batteries_path, scenario)
 
     return network
 
 
-def insert_pv_rooftop(network, shape, buses, pv_rooftop_path, pv_feedin_path):
+def insert_pv_rooftop(network, shape, buses, pv_rooftop_path, pv_feedin_path, scenario):
     solar = gpd.read_file(pv_rooftop_path).to_crs(32632)
     solar = gpd.clip(solar, shape)
 
@@ -31,6 +31,36 @@ def insert_pv_rooftop(network, shape, buses, pv_rooftop_path, pv_feedin_path):
                     """
     )
     solar = solar[solar["distance"] <= 50]
+    
+    #scale down amount of pvs according to scenario values
+    target_capacities = {
+        "Husum_statusQuo": 26.1,  # MWp
+        "Husum_2035": 32.0,
+    } 
+    if scenario not in target_capacities:
+        logging.warning(
+            f"Szenario '{scenario}' not found! "
+            f"Use whole pv-capacity of egon2035 data ({solar['capacity'].sum():.2f} MWp)."
+        )
+        target_p_nom = solar["capacity"].sum()
+    else:
+        target_p_nom = target_capacities[scenario]
+        
+    current_p_nom = solar["capacity"].sum()
+    
+    if current_p_nom > target_p_nom:
+        logging.info(f"Reducing solar rooftop capacity from {current_p_nom:.2f} MWp to {target_p_nom:.2f} MWp")
+        
+        # Datensatz zufällig mischen (reproduzierbar durch random_state)
+        solar = solar.sample(frac=1, random_state=42) 
+        
+        # Kumulierte Summe bilden und filtern
+        solar["cumsum_p_nom"] = solar["capacity"].cumsum()
+        solar = solar[solar["cumsum_p_nom"] <= target_p_nom].copy()
+        
+        logging.info(f"PV capacity reduction finished. Remaining pv: {len(solar)}")
+       
+    
     # insert data into network tables
     solar.rename(columns={"capacity": "p_nom"}, inplace=True)
     solar["Generator"] = solar.apply(lambda b: f"pv_roof_{b.name}_{b.bus}", axis=1)
@@ -65,7 +95,7 @@ def insert_pv_rooftop(network, shape, buses, pv_rooftop_path, pv_feedin_path):
     return network
 
 
-def insert_home_battery(network, shape, buses, batteries_path):
+def insert_home_battery(network, shape, buses, batteries_path, scenario):
     bat = gpd.read_file(batteries_path).to_crs(32632)
     bat = gpd.clip(bat, shape)
     bat = gpd.sjoin_nearest(bat, buses, "left", distance_col="distance")
@@ -77,6 +107,34 @@ def insert_home_battery(network, shape, buses, batteries_path):
                     """
     )
     bat = bat[bat["distance"] <= 50]
+
+    #scale down amount of pvs according to scenario values
+    target_capacities = {
+        "Husum_statusQuo": 2.6,  # MW
+        "Husum_2035": 7.5,
+    } 
+    if scenario not in target_capacities:
+        logging.warning(
+            f"Szenario '{scenario}' not found! "
+            f"Use whole home-batteries capacity of egon2035 data ({bat['p_nom'].sum():.2f} MWp)."
+        )
+        target_p_nom = bat["capacity"].sum()
+    else:
+        target_p_nom = target_capacities[scenario]
+     
+    current_p_nom = bat["p_nom"].sum()
+    
+    if current_p_nom > target_p_nom:
+        logging.info(f"Reducing home-batteries capacity from {current_p_nom:.2f} MW to {target_p_nom:.2f} MW")
+        
+        # Datensatz zufällig mischen (reproduzierbar durch random_state)
+        bat = bat.sample(frac=1, random_state=42) 
+        
+        # Kumulierte Summe bilden und filtern
+        bat["cumsum_p_nom"] = bat["p_nom"].cumsum()
+        bat = bat[bat["cumsum_p_nom"] <= target_p_nom].copy()
+        
+        logging.info(f"Home-batteries capacity reduction finished. Remaining batteries: {len(bat)}")
 
     # insert data into network tables
     bat.rename(columns={"Bus": "bus"}, inplace=True)
